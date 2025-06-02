c1624780659b6789cd1c1994a4209372e15f77b7
Andreas Maier <a.miaer@systems62.com>
2025-06-02 17:21:57 +0200

    changed sql setting

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO FAEHIGKEIT (id, name) VALUES("97444991-5a9e-4ef7-9db9-0889ce3f738e","Schlafen");
-INSERT INTO HELD (id, name, gewicht) VALUES("6c9cda8a-c4f1-43c5-b9f0-95bfb15e67c4","Superheld", "65");
+INSERT INTO FAEHIGKEIT (id, name) VALUES('97444991-5a9e-4ef7-9db9-0889ce3f738e','Schlafen');
+INSERT INTO HELD (id, name, gewicht) VALUES('6c9cda8a-c4f1-43c5-b9f0-95bfb15e67c4','Superheld', '65');
